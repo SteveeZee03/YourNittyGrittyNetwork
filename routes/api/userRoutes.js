7732a668012b43a43b275d67374ffa3bf300c333
ESTEVAN DADDY <enunez391@gmail.com>
2023-04-12 20:52:33 -0700

    added user routes file

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,19 @@
+
+const router = require('express').router();
+const {
+    getUsers,
+    getSingleUser,
+    createSingleUser,
+    updateSingleUser,
+    deleteSingleUser,
+    addSingleFriend,
+    removeSingleFriend,
+} = require ('../../controllers/userController');
+
+
+router.route('/').get(getUsers).post(createSingleUser);
+router.route('/:userId').get(getSingleUser).put(updateSingleUser).delete(deleteSingleUser);
+router.route('/:userId/friends/:friendId').post(addSingleFriend).delete(removeSingleFriend);
+
+
+module.exports = router;
